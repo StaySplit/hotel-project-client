e46270107a18ef1de37b7f910cca8018c3ad8017
unknown <wlals7565@naver.com>
2025-06-13 21:12:43 +0900

    chore: recoil 삭제, zustand 설치, postcss 설치, react-dom 설치, react-router-dom 설치, react-hook-form 설치, react 최신 버전으로 업그레이드, 그 외 자잘한 패키지 업그레이드

--- src/component/main/UserInfo.tsx
+++ src/component/main/UserInfo.tsx
@@ -1,27 +1,15 @@
-import { userSelector } from "@/atoms/userSelector";
-import { useRecoilValueLoadable } from "recoil";
+
 
 const UserInfo = () => {
-  const userLoadable = useRecoilValueLoadable(userSelector);
-
-  if (userLoadable.state === "loading") {
-    return <p>로딩 중...</p>;
-  }
-
-  if (userLoadable.state === "hasError") {
-    return <p>에러 발생: {userLoadable.contents.message}</p>;
-  }
-
-  const user = userLoadable.contents;
 
   return (
     <div className="bg-white dark:bg-gray-900 min-h-screen flex flex-col items-center justify-center space-y-4 p-4">
       <h2 className="text-4xl font-bold text-black dark:text-white">
         👤 유저 정보
       </h2>
-      <p className="font-bold text-black dark:text-white">이름: {user.name}</p>
+      <p className="font-bold text-black dark:text-white">이름: 홍길동</p>
       <p className="font-bold text-black dark:text-white">
-        이메일: {user.email}
+        이메일: 이메일@naver.com
       </p>
     </div>
   );
