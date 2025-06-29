0a9e3bb8dec86e5fa8a3e8834e77f8168a368437
Ajungmiiin <wjdals3249@naver.com>
2025-06-29 21:56:56 +0900

    feat : HeartIcon 컴포넌트 생성

--- src/component/common/icons/HeartIcon.tsx
+++ src/component/common/icons/HeartIcon.tsx
@@ -2,7 +2,7 @@ import { Heart } from 'lucide-react';
 
 interface HeartIconProps {
   like?: boolean;
-  onClick?: () => void;
+  onClick?: (e: React.MouseEvent<SVGSVGElement>) => void;
   className?: string;
   size?: string;
 }
@@ -10,6 +10,9 @@ interface HeartIconProps {
 const HeartIcon = ({ like = false, onClick, className = '', size = '24' }: HeartIconProps) => {
   return (
     <Heart
+      role="button"
+      aria-checked={like}
+      aria-label="찜하기버튼"
       strokeWidth={1}
       fill={like ? '#EC221F' : 'transparent'}
       stroke={like ? '#EC221F' : '#AAAAAA'}
