d3f3d70d66270881e2b17ace1a42dba924dcb3c6
Ajungmiiin <wjdals3249@naver.com>
2025-06-29 21:56:38 +0900

    feat : full Props 추가

--- src/component/common/button/PrimaryButton.tsx
+++ src/component/common/button/PrimaryButton.tsx
@@ -1,29 +1,31 @@
-import React from "react";
+import React from 'react';
 
 interface PrimaryButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   size?: 'sm' | 'md' | 'lg';
   children: React.ReactNode;
+  full?: boolean;
 }
 
 export const PrimaryButton = ({
-  size = "md",
+  size = 'md',
   children,
   disabled = false,
+  full = false,
   ...props
 }: PrimaryButtonProps) => {
   const sizeClass = {
-    sm: "py-1 px-3 text-sm",
-    md: "py-2 px-4 text-base",
-    lg: "py-3 px-6 text-lg",
+    sm: 'py-1.5 px-4 text-sm',
+    md: 'py-2 px-5 text-base',
+    lg: 'py-3 px-6 text-lg',
   }[size];
 
   const baseClass = disabled
-    ? "bg-gray-300 text-white cursor-not-allowed"
-    : "bg-primary-500 text-white hover:bg-primary-600 active:bg-primary-700 cursor-pointer";
+    ? 'bg-gray-300 text-white cursor-not-allowed'
+    : 'bg-primary-500 text-white hover:bg-primary-600 active:bg-primary-700 cursor-pointer';
 
   return (
     <button
-      className={`rounded ${sizeClass} ${baseClass} transition-colors duration-150`}
+      className={`rounded-xl ${sizeClass} ${baseClass} transition-colors duration-150 ${full && 'w-full'}`}
       disabled={disabled}
       {...props}
     >
