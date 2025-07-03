a5a6026d8fdfae20eddabab0fe63dd7c0881bc36
visionSpoiler <wjdgns7565@gmail.com>
2025-07-03 13:24:50 +0900

    chore: 파일 구조 변경

--- src/service/api/hotel/index.ts
+++ src/service/api/hotel/index.ts
@@ -2,10 +2,15 @@ import client from '@/service/instance/client';
 import type Response from '@/types/Responsne';
 import type { IPaginationResult } from '@/types/pageable/pagination-result.interface';
 import type { IHotel } from '@/types/hotel/hotel.interface';
+import type { IHotelDetail } from '@/types/hotel/hotel-detail.interface';
 
-const getHotels = async ({ size, page, sort }: { size: number; page: number; sort: string })=> {
+export const getHotels = async ({ size, page, sort }: { size: number; page: number; sort: string })=> {
     const response = await client.get<Response<IPaginationResult<IHotel>>>(`/hotels/list?page=${page}&size=${size}&sort=${sort}`)
     return response.data
 };
 
-export default getHotels;
+export const getHotelDetail = async (hotelId: number) => {
+    const response = await client.get<Response<IHotelDetail>>(`/hotels/${hotelId}`)
+    return response.data
+}
+
