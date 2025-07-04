7a9531431ea901b2e48fd3a5cd0c359cb7ea2866
visionSpoiler <wjdgns7565@gmail.com>
2025-07-04 14:02:05 +0900

    chore: 라우터 수정

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from 'react-router-dom';
 import HomePage from '@/pages/HomePage';
 import Layout from '@/layout/Layout';
+import HotelsPage from '@/component/hotels/HotelsPage';
+import HotelDetailPage from '@/component/hotel_detail/HotelDetailPage';
+import BookingPage from '@/component/booking/BookingPage';
 
 export const router = createBrowserRouter([
   {
@@ -11,6 +14,20 @@ export const router = createBrowserRouter([
         index: true,
         element: <HomePage />,
       },
+      {
+        path: 'hotels',
+        element: <HotelsPage />
+      }
+      ,
+      {
+        path: 'hotels/:hotelId',
+        element: <HotelDetailPage />
+      }
+      ,
+      {
+        path: 'booking',
+        element: <BookingPage />
+      }
     ],
   },
 ]);
