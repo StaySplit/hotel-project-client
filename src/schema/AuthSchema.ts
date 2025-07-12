d9d62b5d0b95d7d093b0ccb4f458f920e3483688
Ajungmiiin <wjdals3249@naver.com>
2025-07-12 12:39:49 +0900

    fix : RegisterSchema 수정, error message 추가

--- src/schema/AuthSchema.ts
+++ src/schema/AuthSchema.ts
@@ -1,21 +1,14 @@
 import { z } from 'zod';
 
-const UserRole = ['User', 'Company'] as const;
-const Gender = ['Male', 'Female'] as const;
-const LoginSource = ['GOOGLE', 'KAKAO', 'NAVER', 'THIS'] as const;
-
-// phoneNumber Validation
-const phoneRegex = new RegExp(/^01([0|1|6|7|8|9])-([0-9]{3,4})-([0-9]{4})$/);
+const birthdateRegex = /^(19|20)\d{2}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/;
 
 const RequiredValues = [
   'email',
   'password',
-  'username',
+  'name',
   'passwordConfirm',
-  'role',
-  'gender',
-  'phone_number',
-  'nink_name',
+  'nickname',
+  'birthdate',
 ] as const;
 
 export const LoginSchema = z.object({
@@ -25,16 +18,23 @@ export const LoginSchema = z.object({
 
 export const RegisterSchema = z
   .object({
-    email: z.string().email({ message: '잘못된 이메일 형식입니다.' }),
-    username: z.string(),
-    password: z.string().min(10, { message: '비밀번호는 최소 10자 이상이어야 합니다.' }).max(15),
-    passwordConfirm: z.string().min(10).max(15),
-    role: z.enum(UserRole),
-    gender: z.enum(Gender),
-    phone_number: z.string().regex(phoneRegex),
-    nink_name: z.string().min(2, { message: '닉네임은 두글자 이상이어야합니다.' }),
-    image_url: z.string(),
-    login_source: z.enum(LoginSource),
+    email: z
+      .string({ message: '이메일은 필수 입력입니다.' })
+      .email({ message: '잘못된 이메일 형식입니다.' }),
+    name: z.string({ message: '이름은 필수 입력입니다.' }),
+    password: z
+      .string({ message: '비밀번호는 필수 입력입니다.' })
+      .min(10, { message: '비밀번호는 최소 10자 이상이어야 합니다.' }),
+    passwordConfirm: z
+      .string({ message: '비밀번호 확인은 필수 입력입니다.' })
+      .min(10, { message: '비밀번호는 최소 10자 이상이어야 합니다.' }),
+    nickname: z
+      .string({ message: '닉네임은 필수 입력입니다.' })
+      .min(2, { message: '닉네임은 두글자 이상이어야합니다.' })
+      .max(8, { message: '닉네임은 최대 8글자 입니다.' }),
+    birthdate: z
+      .string({ message: '생년월일은 필수 입력입니다.' })
+      .regex(birthdateRegex, '올바른 형식이 아닙니다.'),
   })
   .superRefine((val, ctx) => {
     if (val.password !== val.passwordConfirm) {
