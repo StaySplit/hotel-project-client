6a8b1b413a6f1a67328292963403b0032c47f224
Ajungmiiin <wjdals3249@naver.com>
2025-07-12 12:45:12 +0900

    feat : sign-up page 추가, Provider 적용

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,15 +1,28 @@
 import { createBrowserRouter } from 'react-router-dom';
-import HomePage from '@/pages/HomePage';
+
+import Provider from '@/provider';
 import Layout from '@/layout/Layout';
+
+import HomePage from '@/pages/HomePage';
+
+import SignUpPage from '@/pages/SignUpPage';
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <Layout />,
+    element: (
+      <Provider>
+        <Layout />
+      </Provider>
+    ),
     children: [
       {
         index: true,
         element: <HomePage />,
+      },
+      {
+        path: '/sign-up',
+        element: <SignUpPage />,
       },
     ],
   },
