749b8ecd2c6b58b7b306503032f9335bfe8189a0
Ajungmiiin <wjdals3249@naver.com>
2025-07-25 00:15:03 +0900

    feat : client 인스턴스 baseURL 추가, ngrok-warning 헤더 추가

--- src/service/instance/client.ts
+++ src/service/instance/client.ts
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
 const client = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
+    'ngrok-skip-browser-warning': 'true',
   },
 });
 
