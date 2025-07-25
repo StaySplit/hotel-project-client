b74313f1e29a0dce316fc2c494c104b2fc211064
Ajungmiiin <wjdals3249@naver.com>
2025-07-25 22:56:15 +0900

    feat : useGetInfiniteAllHotels 커스텀 훅 정의

---
+++ src/hooks/queries/hotels/useGetInfiniteHotels.ts
@@ -0,0 +1,18 @@
+import getAllHotels from '@/service/api/hotel/getAllHotels';
+import buildSearchQuery from '@/utils/buildSearchQuery';
+import { useInfiniteQuery } from '@tanstack/react-query';
+
+const useGetInfiniteAllHotels = () => {
+  return useInfiniteQuery({
+    queryKey: ['hotels', 'all'],
+    queryFn: async ({ pageParam }) => {
+      return await getAllHotels(buildSearchQuery({ page: pageParam.toString(), size: '10' }));
+    },
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => {
+      return lastPage.last ? undefined : lastPage.pageable.pageNumber++;
+    },
+  });
+};
+
+export default useGetInfiniteAllHotels;
