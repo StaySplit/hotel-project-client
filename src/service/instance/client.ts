6697ae5be5c8131d266f636094a4d461d4ec1013
Ajungmiiin <wjdals3249@naver.com>
2025-07-25 22:14:52 +0900

    feat : 시군구 json 파일 추가

--- src/service/instance/client.ts
+++ src/service/instance/client.ts
@@ -3,7 +3,6 @@ import axios from 'axios';
 const client = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
-  baseURL: import.meta.env.VITE_API_URI,
   headers: {
     'Content-Type': 'application/json',
     'ngrok-skip-browser-warning': 'true',
