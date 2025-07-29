e6db6d57bce74815b943418d3719fd0cb310df8c
Ajungmiiin <wjdals3249@naver.com>
2025-07-29 17:00:18 +0900

    fix : Hotel 패치 함수 api/hotels 파일로 이동

--- src/service/api/hotel/index.ts
+++ src/service/api/hotel/index.ts
@@ -1,4 +1,8 @@
 import client from '@/service/instance/client';
+import handleApiReqeust from '../handleApiReqeust';
+
+import type { PaginationResult } from '@/types/pageable';
+import type { HotelItem } from '@/types/hotel';
 
 export const getHotels = async ({
   size,
@@ -14,4 +18,18 @@ export const getHotels = async ({
 
 export const getHotelDetail = async (hotelId: number) => {
   return (await client.get(`/hotels/${hotelId}`)).data;
+};
+
+export const getSearchHotels = async (searchQuery: string) => {
+  const response = await handleApiReqeust<PaginationResult<HotelItem>>(() =>
+    client.get(`/api/hotels/search?${searchQuery}`),
+  );
+  return response;
+};
+
+export const getAllHotels = async (searchQuery: string) => {
+  const response = await handleApiReqeust<PaginationResult<HotelItem>>(() =>
+    client.get(`/api/hotels/list?${searchQuery}`),
+  );
+  return response;
 };
