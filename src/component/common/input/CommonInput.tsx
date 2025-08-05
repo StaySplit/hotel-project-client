c58d4e58e1862c827d4a657b74ceb26cc50f1ce0
Ajungmiiin <wjdals3249@naver.com>
2025-08-05 21:25:23 +0900

    fix : focus 상태일 때 border 색상 수정, className Props추가

--- src/component/common/input/CommonInput.tsx
+++ src/component/common/input/CommonInput.tsx
@@ -5,6 +5,7 @@ interface CommonInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   value: string;
   error?: boolean;
   errorMessage?: string;
+  className?: string;
 }
 
 const CommonInput = ({
@@ -15,10 +16,11 @@ const CommonInput = ({
   error = false,
   errorMessage,
   type = 'text',
+  className,
   ...rest
 }: CommonInputProps) => {
   return (
-    <>
+    <div className="w-full">
       {label && (
         <label className="mb-1 block text-black" htmlFor={name}>
           {label}
@@ -31,14 +33,14 @@ const CommonInput = ({
         name={name}
         onChange={onChange}
         value={value}
-        className={`focus:border-primary-300 w-full rounded-xl border px-4 py-2 text-black transition-colors outline-none ${error ? 'border-[#e57373]' : 'border-gray-primary'} ${rest.disabled ? 'cursor-not-allowed bg-gray-100 text-gray-400' : ''}`}
+        className={`focus:${!error ? 'border-primary-300' : 'border-error'} w-full rounded-lg border bg-white px-4 py-2 text-black transition-colors outline-none ${error ? 'border-[#e57373]' : 'border-gray-primary'} ${rest.disabled ? 'cursor-not-allowed bg-gray-100 text-gray-400' : ''} ${className && className}`}
         {...rest}
       />
 
       {error && errorMessage && (
         <span className="text-error inline-block pt-1 text-sm">{errorMessage}</span>
       )}
-    </>
+    </div>
   );
 };
 
