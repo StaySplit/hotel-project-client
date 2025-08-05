ea04d37576defba23db029795083e84bf9a5e4fe
Ajungmiiin <wjdals3249@naver.com>
2025-08-05 21:31:09 +0900

    feat : RegisterHotel 등록 페이지 생성

---
+++ src/pages/RegisterHotelPage.tsx
@@ -0,0 +1,11 @@
+import HotelForm from '@/component/form/HotelForm';
+
+const RegisterHotelPage = () => {
+  return (
+    <section className="h-full flex-1 px-4 pb-4 md:flex md:items-center md:justify-center">
+      <HotelForm />
+    </section>
+  );
+};
+
+export default RegisterHotelPage;
