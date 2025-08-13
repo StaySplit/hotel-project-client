e0f4ffadd46f115b2f914b5406080dc7a8d5d36e
Ajungmiiin <wjdals3249@naver.com>
2025-08-13 23:42:48 +0900

    feat : RegisterHotelPage 디자인 수정, onSubmut 로직 추가

--- src/pages/RegisterHotelPage.tsx
+++ src/pages/RegisterHotelPage.tsx
@@ -1,9 +1,55 @@
-import HotelForm from '@/component/form/HotelForm';
+import type { HotelRegisterType } from '@/schema/HotelSchema';
+
+import getCoordsByAddress from '@/service/api/geocorder/getCoordsByAddress';
+import { registerHotel } from '@/service/api/hotel';
+import { registerPhoto } from '@/service/api/photo';
+
+import buildSearchQuery from '@/utils/buildSearchQuery';
+
+import HotelRegisterForm from '@/component/hotels/register/HotelRegisterForm';
 
 const RegisterHotelPage = () => {
+  const onSubmit = async (data: HotelRegisterType) => {
+    const { lat: latitude, lon: longitude } = await getCoordsByAddress(data.address);
+
+    const response = await registerHotel({
+      name: data.name,
+      address: data.address,
+      starLevel: data.starLevel,
+      description: data.description,
+      latitude,
+      longitude,
+    });
+    console.log(response);
+    const hotelId = response.hotelId;
+    if (hotelId && data.image) {
+      const response_image = await registerPhoto(
+        buildSearchQuery({
+          entityType: 'HOTEL',
+          entityId: '1',
+          displayType: 'MAIN',
+        }),
+        data.image as File,
+      );
+
+      console.log('image :', response_image);
+    }
+  };
+
   return (
-    <section className="h-full flex-1 px-4 pb-4 md:flex md:items-center md:justify-center">
-      <HotelForm />
+    <section className="bg-gray-primary/30 h-vh mx-auto rounded-2xl p-10">
+      <div className="mx-auto flex h-full w-full max-w-[700px] flex-col gap-4">
+        <header>
+          <span className="bg-primary-200 text-primary-600 mb-1 inline-block rounded-full px-3 py-1 text-xs">
+            신규 호텔 등록
+          </span>
+          <h3 className="mb-1 text-3xl font-bold">호텔 등록</h3>
+          <p className="">
+            호텔의 대표 이미지와 기본 정보를 입력하세요. 등록 후에도 언제든지 수정할 수 있습니다.
+          </p>
+        </header>
+        <HotelRegisterForm onSubmit={onSubmit} />
+      </div>
     </section>
   );
 };
