f6920f516e3962e6b946b713ff402fa97e74a665
Ajungmiiin <wjdals3249@naver.com>
2025-08-17 17:43:14 +0900

    fix : hotelSchema 내 image 데이터 타입 수정

--- src/schema/HotelSchema.ts
+++ src/schema/HotelSchema.ts
@@ -4,8 +4,6 @@ const fileSchema = z.custom<File>((v) => v instanceof File && v.size > 0, {
   message: '파일을 선택해주세요.',
 });
 
-const filesSchema = z.array(fileSchema).min(1, '이미지는 최소 1장 업로드해주세요.');
-
 export const hotelSchema = z.object({
   name: z
     .string({ message: '호텔명을 입력해주세요' })
@@ -13,7 +11,7 @@ export const hotelSchema = z.object({
   starLevel: z.string(),
   address: z.string({ message: '주소를 입력해주세요' }).min(1, { message: '주소를 입력해주세요' }),
   description: z.string().min(1, '호텔 설명을 입력해주세요.'),
-  image: z.union([fileSchema, filesSchema]),
+  image: fileSchema,
 });
 
 export type HotelRegisterType = z.infer<typeof hotelSchema>;
