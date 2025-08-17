ae912e18f23a7442f3585e88980afef27e7abd20
Ajungmiiin <wjdals3249@naver.com>
2025-08-17 17:42:42 +0900

    feat : 다중 이미지 업로드 API 추가

--- src/service/api/photo/index.ts
+++ src/service/api/photo/index.ts
@@ -1,19 +1,30 @@
-import axios from 'axios';
 import handleApiReqeust from '../handleApiReqeust';
 import type { PhotoResponse } from '@/types/photo';
-
-const photo = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
-  withCredentials: true,
-  headers: {
-    'Content-Type': 'multipart/form-data',
-  },
-});
+import client from '@/service/instance/client';
 
 export const registerPhoto = async (searchQuery: string, file: File) => {
   console.log(file);
   const response = await handleApiReqeust<PhotoResponse>(() =>
-    photo.post(`/api/photos?${searchQuery}`, { file }),
+    client.post(
+      `/api/photos?${searchQuery}`,
+      { file },
+      { headers: { 'Content-Type': 'multipart/form-data' } },
+    ),
+  );
+
+  return response;
+};
+
+export const registerPhotos = async (searchQuery: string, files: File[]) => {
+  console.log(files);
+  const response = await handleApiReqeust<PhotoResponse>(() =>
+    client.post(
+      `/api/photos/list?${searchQuery}`,
+      { files },
+      {
+        headers: { 'Content-Type': 'multipart/form-data' },
+      },
+    ),
   );
 
   return response;
