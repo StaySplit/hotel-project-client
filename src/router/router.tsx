2d23aa1be9dc879351eec0152c73a0d0bbb8f479
Ajungmiiin <wjdals3249@naver.com>
2025-08-18 23:51:24 +0900

    feat : 호텔 상세페이지 추가

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -3,7 +3,6 @@ import { createBrowserRouter } from 'react-router-dom';
 import Provider from '@/provider/Provider';
 import Layout from '@/layout/Layout';
 import HotelsPage from '@/component/hotels/HotelsPage';
-import HotelDetailPage from '@/component/hotel_detail/HotelDetailPage';
 
 import HomePage from '@/pages/HomePage';
 
@@ -16,6 +15,9 @@ import MyPage from '@/pages/MyPage';
 import ErrorPage from '@/pages/ErrorPage';
 import ProtectedRoute from './ProtectedRouter';
 import ReservationPage from '@/component/mypage/reservation/ReservationPage';
+import HotelDetailPage from '@/component/hotel-detail/HotelDetailPage';
+import RegisterHotelPage from '@/pages/RegisterHotelPage';
+import RegisterRoomPage from '@/pages/RegisterRoomPage';
 
 export const router = createBrowserRouter([
   {
@@ -56,6 +58,18 @@ export const router = createBrowserRouter([
       {
         path: 'hotels/:hotelId',
         element: <HotelDetailPage />,
+      },
+      {
+        path: '/register-room',
+        element: <RegisterRoomPage />,
+      },
+      {
+        path: '/register-hotel',
+        element: (
+          <ProtectedRoute>
+            <RegisterHotelPage />
+          </ProtectedRoute>
+        ),
       },
       {
         path: 'mypage',
