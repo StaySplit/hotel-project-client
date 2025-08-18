221ec53f934aa74e450b3ac7f736e97029d10fb2
Ajungmiiin <wjdals3249@naver.com>
2025-08-18 23:43:02 +0900

    feat : searchHotel, hotelRooms QueryKey 추가

--- src/hooks/queries/hotels/hotelKeys.ts
+++ src/hooks/queries/hotels/hotelKeys.ts
@@ -1,9 +1,9 @@
 import type { SearchType } from '@/schema/SearchSchema';
 
 const hotelKeys = {
-  searchHotels: (searchTerm: SearchType) => ['hotels', 'search', searchTerm],
-  hotelsList: ['hotels', 'all'],
+  searchHotels: (searchTerm: Partial<SearchType>) => ['hotels', 'search', searchTerm],
   hotel: (hotelId: string) => ['hotel', hotelId],
+  hotelRooms: (hotelId: string) => ['hotel', hotelId, 'rooms'],
 };
 
 export default hotelKeys;
