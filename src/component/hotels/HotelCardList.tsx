68b750d881ac5ccf9ef633177e143a874986271a
Ajungmiiin <wjdals3249@naver.com>
2025-08-18 23:54:29 +0900

    feat : HotelCardList 컴포넌트 생성

--- src/component/hotels/HotelCardList.tsx
+++ src/component/hotels/HotelCardList.tsx
@@ -1,12 +1,67 @@
-import React from 'react';
+import { useEffect } from 'react';
+
+import { useSuspenseInfiniteQuery } from '@tanstack/react-query';
+
+import hotelKeys from '@/hooks/queries/hotels/hotelKeys';
+import useObserver from '@/hooks/useObserver';
+
+import { getSearchHotels } from '@/service/api/hotel';
+
+import buildSearchQuery from '@/utils/buildSearchQuery';
+
+import HotelCard from '../card/HotelCard';
+import type { SearchTerm } from '@/layout/SearchLayout';
 
 interface HotelCardListProps {
-  children: React.ReactNode;
-  className?: string;
+  searchTerm: Partial<SearchTerm>;
 }
 
-const HotelCardList = ({ children, className }: HotelCardListProps) => {
-  return <div className={`grid grid-cols-5 gap-4 ${className}`}>{children}</div>;
+const HotelCardList = ({ searchTerm }: HotelCardListProps) => {
+  const { data, hasNextPage, fetchNextPage } = useSuspenseInfiniteQuery({
+    queryKey: hotelKeys.searchHotels(searchTerm),
+    initialPageParam: 0,
+    queryFn: async ({ pageParam }) => {
+      return await getSearchHotels(
+        buildSearchQuery({
+          ...searchTerm,
+          page: pageParam.toString(),
+          size: '10',
+        }),
+      );
+    },
+    getNextPageParam: (lastPage) => {
+      return lastPage.last ? undefined : lastPage.pageable.pageNumber + 1;
+    },
+  });
+
+  const { ref, isView } = useObserver();
+
+  useEffect(() => {
+    if (isView && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [isView, fetchNextPage, hasNextPage]);
+
+  const searchResults = data?.pages.flatMap((data) => data.content) || [];
+
+  return (
+    <>
+      {searchResults.length === 0 && (
+        <p className="flex h-full items-center justify-center">검색 결과가 존재하지 않습니다</p>
+      )}
+      {searchResults.length > 0 && (
+        <ul className="flex flex-col gap-4 lg:grid lg:grid-cols-5">
+          {searchResults.map((hotel) => (
+            <li key={hotel.hotelId} className="w-full">
+              <HotelCard {...hotel} liked={true} handleChangeLike={() => {}} />
+            </li>
+          ))}
+        </ul>
+      )}
+
+      <div className="min-h-[1px] w-full" ref={ref} />
+    </>
+  );
 };
 
 export default HotelCardList;
